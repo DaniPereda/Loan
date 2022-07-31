a8a5352e42c5e0fbc2961b2ec605a1de02a92a59
DaniPereda <109308591+DaniPereda@users.noreply.github.com>
2022-07-31 16:04:44 +0200

    format changes

--- src/main/kotlin/app/LoanService.kt
+++ src/main/kotlin/app/LoanService.kt
@@ -6,21 +6,16 @@ import domain.Loan
 class LoanService(private val dataReader: DataReader) {
     fun start(amount:Float):Loan
     {
-
         var myLoan = Loan(amount)
-
         myLoan.checkValidAmount()
-
         if(!myLoan.denied) {
             var borrowersAvailable = retrieveAll(dataReader)
 
             myLoan.configureBorrowers(borrowersAvailable)
         }
-
         if(!myLoan.denied) {
             myLoan.obtainConditions()
         }
-
         return myLoan
 
 
